25ac03016bd3d0beebca9f57dfa665ab2c02e4ea
Gert Kanter <gert.kanter@ttu.ee>
2016-09-30 01:33:33 +0300

    wat

--- src/main/java/quaternion/QuaternionAngle.java
+++ src/main/java/quaternion/QuaternionAngle.java
@@ -28,7 +28,7 @@ public class QuaternionAngle {
   public double getAngle(Point after, Point axisOfInterest) {
     // axisOfInterest - unit vector, either for X = (1, 0, 0), Y = (0, 1, 0), Z = (0, 0, 1)
     // we are interested how much the coordinate changed on this axis (e.g., if it was pointing to the wall and now is pointing to the ceiling => 90 degrees)
-    return -Math.asin(after.getX() * axisOfInterest.getX() + after.getY() * axisOfInterest.getY() + after.getZ() * axisOfInterest.getZ());
+    return Math.asin(after.getX() * axisOfInterest.getX() + after.getY() * axisOfInterest.getY() + after.getZ() * axisOfInterest.getZ()); // siin läheb märk ka pekki veel :P
   }
 
 }
